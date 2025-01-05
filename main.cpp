2652bd09ce76e295f8667426521e2daacf2d9201
ValetteEvan <Valetteevan23@gmail.com>
2025-01-05 18:48:26 +0100

    add

--- main.cpp
+++ main.cpp
@@ -1,68 +1,17 @@
 #include <iostream>
-#include <ostream>
+#include <vector>
+#include <span>
 
-template<typename T>
-T add(T a,T b)
-{
-    return a + b;
-}
-
-template<typename T>
-class Box 
-{
-    T value;
-    public:
-    Box(T _value) : value(_value) {} 
-    T getValue()const {return value;}
-};
-
-template<>
-class Box<int>
-{
-    int value;
-    public:
-    Box(int _value) : value(_value) {} 
-    int getValue()const {return value;}
-    void printTypeOfValue() {std::cout << "Integer" << std::endl;}
-};
-
-template<>
-class Box<float>
-{
-    float value;
-    public:
-    Box(float _value) : value(_value) {} 
-    float getValue()const {return value;}
-    void printTypeOfValue() {std::cout << "Float" << std::endl;}
-};
-template<typename T>
-std::ostream& operator<<(std::ostream& os,const Box<T>& box)
-{
-    os << box.getValue();
-    return os;
-}
-
-//i love template
-template<typename... Args> 
-void printf(Args... args)
-{
-    (std::cout << ... << args) << std::endl; 
-}
 
 int main()
 {
-  Box<int> boxInt(5);
-  std::cout << boxInt.getValue() << std::endl;
-  boxInt.printTypeOfValue();
+    int arr[] = {1,2,3,4,5};
+    std::span<int> s(arr);  
 
-  Box<float> boxFloat(23.788);
-  std::cout << boxFloat.getValue() << std::endl;
-  boxFloat.printTypeOfValue();  
-
-  Box<std::string> boxString("Hello");
-  std::cout << boxString.getValue() << std::endl;
-
-  printf(boxString,"-",boxFloat,"-",boxInt);
+    for (int val : s)
+    {
+        std::cout << val << " ";
+    }
 
     return 0;
 }
